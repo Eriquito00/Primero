eeb374b78a6ad7d248cea9e708fd10d4a4d4c257
Eriquito00 <150047758+Eriquito00@users.noreply.github.com>
2026-03-10 13:47:16 +0100

    JS transpilation

--- frontend/public/js/components/Card.js
+++ frontend/public/js/components/Card.js
@@ -1,13 +1,24 @@
 import { addAtributes, addText, createElement } from "../libs/lib_element.js";
 export class Card extends HTMLElement {
-    constructor() {
-        super();
-    }
     createCard(color, number) {
-        const div = createElement("div");
-        addAtributes(div, { class: `card card_${color.toLowerCase()}` });
-        addText(div, number.toString());
-        return div;
+        const card = createElement("div");
+        addAtributes(card, {
+            class: `card card_${color.toLowerCase()}`,
+            draggable: "true"
+        });
+        const cornerTop = createElement("div");
+        addAtributes(cornerTop, { class: "card_corner card_corner_top" });
+        addText(cornerTop, number.toString());
+        const center = createElement("div");
+        addAtributes(center, { class: "card_center" });
+        addText(center, number.toString());
+        const cornerBottom = createElement("div");
+        addAtributes(cornerBottom, { class: "card_corner card_corner_bottom" });
+        addText(cornerBottom, number.toString());
+        card.appendChild(cornerTop);
+        card.appendChild(center);
+        card.appendChild(cornerBottom);
+        return card;
     }
 }
 customElements.define("primero-card", Card);
