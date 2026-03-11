643f431beda91f5564bc1ab2bf610963577f5f9d
Eriquito00 <150047758+Eriquito00@users.noreply.github.com>
2026-03-11 13:35:03 +0100

    Card eventlisteners

--- frontend/src/components/Card.ts
+++ frontend/src/components/Card.ts
@@ -1,32 +1,71 @@
 import { addAtributes, addText, createElement } from "../libs/lib_element.js";
 
-export class Card extends HTMLElement {
+export let draggedCard: HTMLElement | null = null;
 
-    public createCard(color: string, number: number) {
+export class Card extends HTMLElement {
+    private color: string;
+    private number: number;
+
+    public constructor(color: string, number: number) {
+        super();
+        this.color = color;
+        this.number = number;
+    }
+
+    public createCard() {
 
         const card = createElement("div");
         addAtributes(card, {
-            class: `card card_${color.toLowerCase()}`,
+            class: `card card_${this.color.toLowerCase()}`,
             draggable: "true"
         });
 
+        card.addEventListener("dragstart", (e) => {
+            draggedCard = card;
+            e.dataTransfer!.setData("application/json", JSON.stringify(this.getCardData()));
+        });
+
+        card.addEventListener("dragend", () => {
+            draggedCard = null;
+        });
+
+        card.addEventListener("mouseenter", (e) => { this.cardEnter(e); });
+        card.addEventListener("dragenter", (e) => { this.cardEnter(e); });
+        card.addEventListener("mouseleave", (e) => { this.cardLeave(e); });
+        card.addEventListener("dragleave", (e) => { this.cardLeave(e); });
+
         const cornerTop = createElement("div");
         addAtributes(cornerTop, { class: "card_corner card_corner_top" });
-        addText(cornerTop, number.toString());
+        addText(cornerTop, this.number.toString());
 
         const center = createElement("div");
         addAtributes(center, { class: "card_center" });
-        addText(center, number.toString());
+        addText(center, this.number.toString());
 
         const cornerBottom = createElement("div");
         addAtributes(cornerBottom, { class: "card_corner card_corner_bottom" });
-        addText(cornerBottom, number.toString());
+        addText(cornerBottom, this.number.toString());
 
         card.appendChild(cornerTop);
         card.appendChild(center);
         card.appendChild(cornerBottom);
 
         return card;
+    }
+
+    private cardEnter(e: Event) {
+        (e.target as HTMLElement).classList.add("card_drag");
+    }
+
+    private cardLeave(e: Event) {
+        (e.target as HTMLElement).classList.remove("card_drag");
+    }
+
+    public getCardData() {
+        return {
+            "color": this.color,
+            "number": this.number
+        }
     }
 }
 
