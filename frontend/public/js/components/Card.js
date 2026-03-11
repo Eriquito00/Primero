3dc52358472b96db24e2fa2291efeb2535c01176
Eriquito00 <150047758+Eriquito00@users.noreply.github.com>
2026-03-11 13:35:30 +0100

    JS transpilated

--- frontend/public/js/components/Card.js
+++ frontend/public/js/components/Card.js
@@ -1,24 +1,55 @@
 import { addAtributes, addText, createElement } from "../libs/lib_element.js";
+export let draggedCard = null;
 export class Card extends HTMLElement {
-    createCard(color, number) {
+    color;
+    number;
+    constructor(color, number) {
+        super();
+        this.color = color;
+        this.number = number;
+    }
+    createCard() {
         const card = createElement("div");
         addAtributes(card, {
-            class: `card card_${color.toLowerCase()}`,
+            class: `card card_${this.color.toLowerCase()}`,
             draggable: "true"
         });
+        card.addEventListener("dragstart", (e) => {
+            draggedCard = card;
+            e.dataTransfer.setData("application/json", JSON.stringify(this.getCardData()));
+        });
+        card.addEventListener("dragend", () => {
+            draggedCard = null;
+        });
+        card.addEventListener("mouseenter", (e) => { this.cardEnter(e); });
+        card.addEventListener("dragenter", (e) => { this.cardEnter(e); });
+        card.addEventListener("mouseleave", (e) => { this.cardLeave(e); });
+        card.addEventListener("dragleave", (e) => { this.cardLeave(e); });
         const cornerTop = createElement("div");
         addAtributes(cornerTop, { class: "card_corner card_corner_top" });
-        addText(cornerTop, number.toString());
+        addText(cornerTop, this.number.toString());
         const center = createElement("div");
         addAtributes(center, { class: "card_center" });
-        addText(center, number.toString());
+        addText(center, this.number.toString());
         const cornerBottom = createElement("div");
         addAtributes(cornerBottom, { class: "card_corner card_corner_bottom" });
-        addText(cornerBottom, number.toString());
+        addText(cornerBottom, this.number.toString());
         card.appendChild(cornerTop);
         card.appendChild(center);
         card.appendChild(cornerBottom);
         return card;
+    }
+    cardEnter(e) {
+        e.target.classList.add("card_drag");
+    }
+    cardLeave(e) {
+        e.target.classList.remove("card_drag");
+    }
+    getCardData() {
+        return {
+            "color": this.color,
+            "number": this.number
+        };
     }
 }
 customElements.define("primero-card", Card);
